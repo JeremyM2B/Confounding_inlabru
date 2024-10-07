2c0c7287790af81465b73356c523fe1e42596e60
AzGeffroy <alizee.geffroy@gmail.com>
2024-10-07 18:05:36 +0200

    ♻️  refactor code

--- Application.R
+++ Application.R
@@ -11,11 +11,7 @@ library(fmesher)
 library(INLA)
 library(inlabru)
 library(INLAutils)
-# Note: the 'splancs' package also needs to be installed,
-# but doesn't need to be loaded
 
-# Bibliothèques
-source(file.path("code", "function.R", fsep = "/"))
 # set option
 select <- dplyr::select
 options(scipen = 99999)
@@ -33,41 +29,16 @@ df_FRANCE_count <- df_FRANCE_subset %>%
     st_transform("EPSG:2154")
 df_FRANCE_count$site_idx <- row.names(df_FRANCE_count)
 
-
-# # Charger les packages
-# library(spdep)
-# library(sf)
-# library(dplyr)
-
-# # Charger les données spatiales
-# df_FRANCE_count <- df_FRANCE_count %>%
-#     st_as_sf(coords = c("lon", "lat"), crs = 4326, remove = FALSE) %>%
-#     st_transform("EPSG:2154")
-
-# # Calculer la matrice des voisins basée sur une distance
-# coords <- st_coordinates(df_FRANCE_count)
-# nb <- dnearneigh(coords, 0, 100000)  # Ajustez la distance selon vos besoins
-
-# # Calculer les poids spatiaux
-# listw <- nb2listw(nb, style = "W")
-
-# # Calculer l'indice de Moran pour Cd_mousse
-# moran_test <- moran.test(df_FRANCE_count$Cd_mousse, listw)
-
-# # Afficher les résultats
-# print(moran_test)
-
-
-
 FRANCE <- terra::vect("data/regbiofr/region_biogeographique.shp")
 France <- FRANCE[FRANCE$CODE == "ALP" | FRANCE$CODE == "CON" | FRANCE$CODE == "ATL" | FRANCE$CODE == "MED"]
 
 load(file.path(rep, "df_france_cd_air_2016", fsep = "/"))
 cd_air_2016 <- df_france_cd_air_2016 %>% select(-c("i", "j"))
 cd_air_2016_rast <- terra::rast(cd_air_2016,
-                                type = "xyz",
-                                crs = "EPSG:4326",
-                                digits = 6) %>% 
+    type = "xyz",
+    crs = "EPSG:4326",
+    digits = 6
+) %>%
     terra::project("EPSG:2154")
 cd_air_2016_rast <- terra::crop(cd_air_2016_rast, France, mask = TRUE)
 r2 <- cd_air_2016_rast
@@ -105,12 +76,13 @@ coords <- st_coordinates(df_FRANCE_count)
 bdry <- inla.sp2segment(st_as_sf(as.polygons(cd_air_2016_rast)))
 bdry$loc <- inla.mesh.map(bdry$loc)
 new_mesh <- inla.mesh.2d(
-                         loc = coords,
-                         boundary = bdry,
-                         max.edge = c(100000, 170000), # km inside and outside
-                         cutoff = 2000,
-                         offset = c(100000, 170000),
-                         crs = fm_crs(as.matrix(df_FRANCE_count)))
+    loc = coords,
+    boundary = bdry,
+    max.edge = c(100000, 170000), # km inside and outside
+    cutoff = 2000,
+    offset = c(100000, 170000),
+    crs = fm_crs(as.matrix(df_FRANCE_count))
+)
 
 # Mesh plot with measure sites
 ggplot() +
@@ -128,7 +100,7 @@ spde <- inla.spde2.pcmatern(
 )
 
 
-################################# Models and methods#######################################
+################################# Models implementation #######################################
 
 ############# Null model
 
@@ -142,8 +114,9 @@ res_null <- bru(
         verbose = FALSE
     )
 )
-summary(res_null)
-############# Standard spatial model
+# summary(res_null)
+
+############# Spatial model
 
 res_spatial <- bru(
     components = log(Cd_mousse) ~ -1 + intercept(1) + EMEP_air + u(geometry, model = spde),
@@ -155,9 +128,26 @@ res_spatial <- bru(
         verbose = FALSE
     )
 )
-summary(res_spatial)
+# summary(res_spatial)
 
-############# Spatial+ v1 model with EMEP fitted with bru
+############# RSR model
+
+res_RSR_with_i <- bru(
+    components = ~ -1 + intercept(1) + X_Effect(EMEP_air) + u(geometry, model = spde),
+    like(
+        formula = log(Cd_mousse) ~ 0 + intercept + X_Effect + u - (sum(u) * sum(EMEP_air^2) + EMEP_air * (length(EMEP_air) * sum(EMEP_air * u) - sum(u) * sum(EMEP_air)) - sum(EMEP_air * u) * sum(EMEP_air)) / (length(EMEP_air) * sum(EMEP_air^2) - sum(EMEP_air)^2),
+        family = "gaussian",
+        data = df_FRANCE_count
+    ),
+    options = list(
+        control.compute = list(waic = TRUE, cpo = FALSE),
+        control.inla = list(int.strategy = "eb"),
+        verbose = FALSE
+    )
+)
+# summary(res_RSR_with_i)
+
+############# Spatial+ model
 spde_X <- spde <- inla.spde2.pcmatern(
     mesh = new_mesh,
     prior.range = c(130000, 0.05), # P(range < 130000) = 0.05
@@ -180,7 +170,7 @@ res_EMEP <- bru(
 
 true_EMEP <- df_FRANCE_count$EMEP_air
 
-df_FRANCE_count$fitted_EMEP_air_bru <- true_EMEP - 
+df_FRANCE_count$fitted_EMEP_air_bru <- true_EMEP -
     res_EMEP$summary.fitted.values[0:445, ]$mean
 
 res_spatial_plus1 <- bru(
@@ -197,7 +187,7 @@ res_spatial_plus1 <- bru(
         verbose = FALSE
     )
 )
-summary(res_spatial_plus1)
+# summary(res_spatial_plus1)
 
 ############# Spatial+ v1 model with EMEP fitted with lm
 mod_EMEP_lm <- lm(EMEP_air ~ -1 + lon + lat, data = df_FRANCE_count)
@@ -228,14 +218,9 @@ summary(res_spatial_plus1_lm)
 
 library(mgcv)
 
+k_sp <- 60
 
-k_sp=60
-
-mod_EMEP_gam <-gam(EMEP_air ~ -1 + s(lon,lat, k=60), data=df_FRANCE_count)
-# b<-mod_EMEP_gam
-# plot(b,pages=1,residuals=TRUE)
-# gam.check(b)
-# summary(mod_EMEP_gam)
+mod_EMEP_gam <- gam(EMEP_air ~ -1 + s(lon, lat, k = 60), data = df_FRANCE_count)
 
 df_FRANCE_count$fitted_EMEP_air_gam <- true_EMEP - mod_EMEP_gam$fitted.values
 
@@ -256,19 +241,19 @@ res_spatial_plus1_gam_bru <- bru(
 summary(res_spatial_plus1_gam_bru)
 
 res_spatial_plus1_gam_gam <- gam(
-                                 log(Cd_mousse) ~ fitted_EMEP_air_gam +
-                                     s(lon, lat, k = 160),
-                                 data = df_FRANCE_count)
+    log(Cd_mousse) ~ fitted_EMEP_air_gam +
+        s(lon, lat, k = 160),
+    data = df_FRANCE_count
+)
 summary(res_spatial_plus1_gam_gam)
 
-###### Spatial+ 2.0 model
+############# Spatial+ 2.0 model
 hyperparam <- res_spatial$summary.hyperpar
 log_range_u <- hyperparam["Range for u", "mean"]
 
 dist <- sp::spDists(coordinates(df_FRANCE_subset[, c("lon", "lat")]), longlat = TRUE)
 
 Sigma <- inla.matern.cov(nu = 1, kappa = sqrt(8) / log_range_u, dist)
-# Q <- inla.spde2.precision(spde = spde, theta = c(log_range_u, log_sigma_u))
 Q <- solve(Sigma)
 r <- eigen(Q) # spectral decomposition
 eigen.vect <- r$vectors # eigen vectors
@@ -278,17 +263,19 @@ coef <- solve(eigen.vect, df_FRANCE_count$EMEP_air)
 
 fit_with_eigen_vectors <- function(nbr_eigen_vectors = 445,
                                    data = df_FRANCE_count,
-                                   decomposition=coef) {
+                                   decomposition = coef) {
     col_name <- paste("EMEP_air.eigen", nbr_eigen_vectors, sep = "")
-    if(nbr_eigen_vectors == 1){
-        data[, col_name] <-  as.vector(eigen.vect[, 0:nbr_eigen_vectors] * decomposition[0:nbr_eigen_vectors])
-    }else{
-        data[, col_name] <-  as.vector(eigen.vect[, 0:nbr_eigen_vectors]%*%decomposition[0:nbr_eigen_vectors])
+    if (nbr_eigen_vectors == 1) {
+        data[, col_name] <- as.vector(eigen.vect[, 0:nbr_eigen_vectors] * decomposition[0:nbr_eigen_vectors])
+    } else {
+        data[, col_name] <- as.vector(eigen.vect[, 0:nbr_eigen_vectors] %*% decomposition[0:nbr_eigen_vectors])
     }
     res <- bru(
         components = as.formula(
-          paste0("~ -1 + intercept(1) + ",
-                 col_name, " + u(geometry, model = spde)", sep = "")
+            paste0("~ -1 + intercept(1) + ",
+                col_name, " + u(geometry, model = spde)",
+                sep = ""
+            )
         ),
         like(
             formula = log(Cd_mousse) ~ .,
@@ -304,26 +291,5 @@ fit_with_eigen_vectors <- function(nbr_eigen_vectors = 445,
     return(res)
 }
 
-res_spatial_plus2 <- fit_with_eigen_vectors(341)
-summary(res_spatial_plus2)
-
-#####RSR
-spde_RSR <- inla.spde2.pcmatern(
-    mesh = new_mesh,
-    prior.range = c(18000000, 0.9999), # P(range < 18000000) = 0.9999
-    prior.sigma = c(2.5, 0.0001) # P(sigma > 2.5) = 0.0001,
-)
-
-res_RSR_with_i <- bru( components = ~ -1 + intercept(1) + X_Effect(EMEP_air) + u(geometry, model = spde_RRS),
-    like(
-        formula = log(Cd_mousse) ~ 0 + intercept + X_Effect + u  - (sum(u) * sum(EMEP_air^2) + EMEP_air * (length(EMEP_air) * sum(EMEP_air * u) - sum(u) * sum(EMEP_air)) - sum(EMEP_air * u) * sum(EMEP_air)) / (length(EMEP_air) * sum(EMEP_air^2) - sum(EMEP_air)^2), 
-        family = "gaussian",
-        data = df_FRANCE_count
-    ),
-    options = list(
-            control.compute = list(waic = TRUE, cpo = FALSE),
-            control.inla = list(int.strategy = "eb"),
-            verbose = FALSE
-        )
-)
-summary(res_RSR_with_i)
+res_spatial_plus2 <- fit_with_eigen_vectors(310)
+# summary(res_spatial_plus2)
